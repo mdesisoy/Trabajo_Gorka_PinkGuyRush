325754e0d1ac6e1de58614546cf708559bb31473
daniel <tomas.madan21@zaragoza.salesianos.edu>
2022-05-07 19:03:51 +0200

    añadiendo primera fase de generador de bolas
    aun esta en fase temprana y tienen algun error que se solventara pronto
    :)

--- Proyecto_Prueba/cañon.gd
+++ Proyecto_Prueba/cañon.gd
@@ -7,12 +7,10 @@ var preBola = preload ("res://final/assets/bola.tscn")
 
 func _physics_process(delta):
 	look_at(player.get_position())
-	$Light2D/AnimationPlayer.play("color")
-	$Light2D/AnimationPlayer2.play("luz")
 	distance2player = self.global_position.distance_to(player.global_position)
 
 func _on_Timer_timeout():
-	if distance2player < 400:
+	if distance2player < 480:
 		var bola = preBola.instance()
 		get_parent().call_deferred("add_child", bola)
 		bola.position.x = self.position.x
