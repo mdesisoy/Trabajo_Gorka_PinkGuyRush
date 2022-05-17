3422d8927d99d9606fac3b81be731bb22760465b
mdesisoy <33655393+mdesisoy@users.noreply.github.com>
2022-05-17 20:11:18 +0200

    Cambios en Botones de menu y Niveles

--- scripts/BotonesMenu.gd
+++ scripts/BotonesMenu.gd
@@ -1,14 +1,16 @@
 extends Control
 
 var music_on = true
+onready var opciones = $Opciones
 
 func _on_Salir_pressed():
 	get_tree().quit()
 
+func _on_Opciones_pressed():
+	opciones.popup_centered()
 
 func _on_Jugar_pressed():
 	get_tree().change_scene("res://escenas/Niveles.tscn")
-
 
 
 func _on_TextureButton_button_down():
@@ -17,4 +19,3 @@ func _on_TextureButton_button_down():
 		get_tree().get_nodes_in_group("bgm")[0].volume_db = -12.235
 	else:
 		get_tree().get_nodes_in_group("bgm")[0].volume_db = -80
-
