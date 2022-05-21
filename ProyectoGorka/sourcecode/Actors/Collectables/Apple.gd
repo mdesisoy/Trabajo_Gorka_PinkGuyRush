c46d1ecd527119d9a9f42cd106d4060555d2b88b
pvillarte <pablovillarte@gmail.com>
2022-05-21 13:39:20 +0200

    Añadido nivel2, enemigos al 1  y solucion de errores en lvl1

--- ProyectoGorka/sourcecode/Actors/Collectables/Apple.gd
+++ ProyectoGorka/sourcecode/Actors/Collectables/Apple.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+var taken = false
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("player"):
-		get_parent().get_parent().puntos += 1
-		body.amarillo()
-		$AnimatedSprite.play("collected")
-		$AudioStreamPlayer2D.play()
+	if !taken:
+		if body.is_in_group("player"):
+			taken = true
+			get_parent().get_parent().puntos += 1
+			body.amarillo()
+			$AnimatedSprite.play("collected")
+			$AudioStreamPlayer2D.play()
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "collected":
--- ProyectoGorka/sourcecode/Actors/Collectables/Banana.gd
+++ ProyectoGorka/sourcecode/Actors/Collectables/Banana.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("player"):
-		get_parent().get_parent().puntos += 2
-		body.amarillo()
-		$AnimatedSprite.play("collected")
-		$AudioStreamPlayer2D.play()
+var taken = false
 
+func _on_Area2D_body_entered(body):
+	if !taken:
+		if body.is_in_group("player"):
+			taken = true
+			get_parent().get_parent().puntos += 2
+			body.amarillo()
+			$AnimatedSprite.play("collected")
+			$AudioStreamPlayer2D.play()
 
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation == "collected":
